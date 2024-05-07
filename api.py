2a518efb6efade64ea892d5aeb1426121790054d
Gustavo <gu.farias16@gmail.com>
2024-05-07 09:04:15 -0300

    criando api

---
+++ api.py
@@ -0,0 +1,19 @@
+from flask import blueprint 
+
+bp = blueprint("api", __name__)
+
+
+#Daqui pra baixo as rotas
+
+@bp.route('/')
+def index():
+    return jsonify({"status": 200, "message": "API do Gustavo Da silva Castro Farias"})
+@bp.route("/aleatorios")
+def aleatorios():
+    import random
+    a = random.randint(49, 100)
+    return jsonify({"status": 200, "data": a})
+
+@bp.route("/argumentos/<string:nome>")
+def argumentos(nome: str):
+    return jsonify({"status": 200, "data": nome})
